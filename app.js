f15a27fad3b65524a4ba6da6c2ed9b2ae6bae665
kalpataru <pradhankalpataru2002@gmail.com>
2026-01-09 11:13:54 +0530

    Fix currUser ReferenceError in EJS

--- app.js
+++ app.js
@@ -86,9 +86,10 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
+  res.locals.currUser = req.user || null;
   next();
 });
+
 
 
 app.use("/", userRouter);
